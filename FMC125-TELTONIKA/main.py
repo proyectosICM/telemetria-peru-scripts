2d6daf4d58995ddb65b2ed97d3e802bd2c50a05b
Eduardo Aguilar <es123y123@gmail.com>
2024-09-17 15:36:21 -0500

    Bugg soolucionados

--- FMC125-TELTONIKA/main.py
+++ FMC125-TELTONIKA/main.py
@@ -1,10 +1,12 @@
 import argparse
 from socket_server import start_server
 from gui import run_gui  # Asume que importas la GUI aquí
+from threading import Event
 
 def run_from_console():
-    start_server()
-
+    stop_event = Event()  # Crea el evento stop_event
+    start_server(stop_event)  # Pasa el evento a start_server
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Inicia el servidor en modo consola o GUI.")
     parser.add_argument('--gui', action='store_true', help="Ejecutar en modo gráfico")
