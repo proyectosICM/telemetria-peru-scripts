2076aa464bd8ea1760d49be6a8d006f285087aad
Eduardo Aguilar <es123y123@gmail.com>
2024-11-12 10:04:50 -0500

    c

--- FMC125-TELTONIKA/socket_server.py
+++ FMC125-TELTONIKA/socket_server.py
@@ -3,6 +3,7 @@ from parse_codec8_extended import parse_codec8_extended
 from mqtt_manager import send_to_mqtt
 import json
 from threading import Event
+import struct 
 
 # Optional debugging lines: Uncomment lines 24, 45, 46, 53, 56, 67, 72, 83, and 84
 # to view additional output in the console and help trace execution.
@@ -10,28 +11,36 @@ from threading import Event
 def start_server(stop_event, host='0.0.0.0', port=9526):
     # Create the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Configuración adicional para evitar errores de "Address already in use"
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Permitir reutilización del puerto
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))  # Liberar puerto inmediatamente
+    
     server_socket.bind((host, port))
     server_socket.listen(1)
     print(f"Server listening on {host}:{port}")
     
     # Set a timeout to check the stop signal
     server_socket.settimeout(1)  
-
-    while not stop_event.is_set():
-        try:
+    try: 
+        while not stop_event.is_set():
             try:
-                client_socket, client_address = server_socket.accept()
-                # print(f"Connection established with {client_address}")
+                try:
+                    client_socket, client_address = server_socket.accept()
+                    # print(f"Connection established with {client_address}")
 
-                # Start processing client data
-                handle_client(client_socket, client_address)
+                    # Start processing client data
+                    handle_client(client_socket, client_address)
 
-            except socket.timeout:
-                # Continue on timeout, allowing the loop to run as long as the server is not stopped
-                continue
-        except Exception as e:
-            print(f"Server error: {e}")
-    
+                except socket.timeout:
+                    # Continue on timeout, allowing the loop to run as long as the server is not stopped
+                    continue
+            except Exception as e:
+                print(f"Server error: {e}")
+    finally:
+        # Cerrar el socket del servidor cuando se detiene
+        server_socket.close()
+        print("Server stopped.")
     # Close the server socket when stopped
     server_socket.close()
     print("Server stopped.")
