b880bc9c62f8df0dfe0bfd0c4dc4de9195c8d591
Eduardo-Aguilar-21 <104830939+Eduardo-Aguilar-21@users.noreply.github.com>
2026-03-04 18:05:43 -0500

    Fix indentation in mqtt_manager.py

--- FMC125-TELTONIKA/mqtt_manager.py
+++ FMC125-TELTONIKA/mqtt_manager.py
@@ -5,6 +5,7 @@ import time
 def on_connect(client, userdata, flags, rc):
     pass  
     #print(f"Connected with result code {rc}")
+    #
 
 def send_to_mqtt(data, broker="192.168.0.204", port=1883, topic="prueba"):
     def on_publish(client, userdata, mid):
